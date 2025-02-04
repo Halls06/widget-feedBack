f096f5ecd05271d17823ff15a9f1d19055c15401
Halls06 <antoniomotoe4plus@gmail.com>
2025-02-03 21:10:20 -0300

    first-commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react"
+import { ChatTeardropDots} from "@phosphor-icons/react"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export function App(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Popover className="absolute bottom-4 right-4">
+      <PopoverPanel>Comente Aqui</PopoverPanel>
+      <PopoverButton className="bg-violet-600 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6"/>
+      <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+        <span className="pl-2">
+          FeedBack
+        </span>
+      </span>
+      </PopoverButton>
+    </Popover>
   )
 }
-
-export default App
