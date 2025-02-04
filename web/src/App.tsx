aed59ff4b169ec07ab733b2cd31ec8667f17e975
Halls06 <antoniomotoe4plus@gmail.com>
2025-02-04 15:09:13 -0300

    :tada

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,7 @@
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react"
-import { ChatTeardropDots} from "@phosphor-icons/react"
-
+import { WidgetButton } from "./components/WidgetButton";
 
 export function App(){
   return (
-    <Popover className="absolute bottom-4 right-4">
-      <PopoverPanel>Comente Aqui</PopoverPanel>
-      <PopoverButton className="bg-violet-600 rounded-full px-3 h-12 text-white flex items-center group">
-        <ChatTeardropDots className="w-6 h-6"/>
-      <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-        <span className="pl-2">
-          FeedBack
-        </span>
-      </span>
-      </PopoverButton>
-    </Popover>
+    <WidgetButton />
   )
 }
---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,36 @@
+import { CloseButton } from "./CloseButton";
+
+
+const feedbackTypes = {
+    bug:{
+      title: 'Problema'  
+    },
+    IDEIA: {
+        title: 'Ideia'
+    },
+    OTHER: {
+        title: 'Outro'
+    },
+}
+
+
+export function WidgetForm(){
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100-vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+
+            </div>
+
+
+            <footer className="text-xs text-neutral-400">
+                Feito com &#128151; pela  <a className="underline underline-offset-2" href="#">CodebyHalls</a>
+            </footer>
+
+        </div>
+    );
+}
